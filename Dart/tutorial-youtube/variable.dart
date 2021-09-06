1782aed950f6ae7ef9aef8cfffbac298cc7b9b2a
YousudAzadAyon <52939358+yousufAzadAyon@users.noreply.github.com>
2021-09-07 02:49:59 +0600

    variable

---
+++ Dart/tutorial-youtube/variable.dart
@@ -0,0 +1,9 @@
+void main() {
+  String name = "alpayon";
+  int age = 25;
+  double gpa = 3.49;
+  bool isRegistaredVoter = true;
+
+  print("his name was $name and he was $age years old");
+  print("his gpa in bachelors was $gpa and was he a voter? $isRegistaredVoter");
+}
