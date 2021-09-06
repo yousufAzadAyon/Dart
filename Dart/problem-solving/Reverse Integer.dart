501106d24dbc3207a9e4cf8ea15109442c0879f5
YousudAzadAyon <52939358+yousufAzadAyon@users.noreply.github.com>
2021-09-07 02:52:57 +0600

    reverse integer problem from leetcode

---
+++ Dart/problem-solving/Reverse Integer.dart
@@ -0,0 +1,25 @@
+class reverseSolution {
+  reverseIntegerWithString(int number) {
+    String temp = number.toString();
+    List gg = [];
+    for (var i = temp.length - 1; i >= 0; i--) {
+      gg.add(temp[i]);
+    }
+    String tempString = gg.join();
+    return int.parse(tempString);
+  }
+
+  reverseIntegerWithMod(int number) {
+    int rev_number = 0;
+    while (number > 0) {
+      rev_number = rev_number * 10 + number % 10;
+      number = number ~/ 10;
+    }
+    return rev_number;
+  }
+}
+
+main(List<String> args) {
+  var testing = reverseSolution();
+  print(testing.reverseIntegerWithMod(444555666777888999));
+}
