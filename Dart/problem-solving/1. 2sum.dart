56201fc733f47cb44c46d0bbf9958296d0b1d19b
YousudAzadAyon <52939358+yousufAzadAyon@users.noreply.github.com>
2021-09-08 00:34:32 +0600

    renamed

---
+++ Dart/problem-solving/1. 2sum.dart
@@ -0,0 +1,20 @@
+class TwoSumSolution {
+  Set twoSum(List mylist, int target) {
+    for (var i = 0; i < mylist.length; i++) {
+      for (var j = i + 1; j < mylist.length; j++) {
+        if (mylist[i] + mylist[j] == target) {
+          print("index are:");
+          return {i, j};
+        }
+      }
+    }
+    return {};
+  }
+}
+
+main() {
+  var testing = TwoSumSolution();
+  List<int> myList = [2, 3, 4, 5];
+
+  print(testing.twoSum(myList, 9));
+}
