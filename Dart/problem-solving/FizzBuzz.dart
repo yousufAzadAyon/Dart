cca94141b4e713e4346a8fad26ea71f2da580f68
YousudAzadAyon <52939358+yousufAzadAyon@users.noreply.github.com>
2021-09-17 02:56:13 +0600

    fizzBuzz Lol

---
+++ Dart/problem-solving/FizzBuzz.dart
@@ -0,0 +1,20 @@
+class Solution {
+  fizzBuzz(int limit) {
+    for (int i = 0; i < limit; i++) {
+      if ((i % 3 == 0) & (i % 5 == 0)) {
+        print("fizzBuzz");
+      } else if (i % 3 == 0) {
+        print("fizz");
+      } else if (i % 5 == 0) {
+        print("buzz");
+      } else {
+        print(i);
+      }
+    }
+  }
+}
+
+main(List<String> args) {
+  var testing = Solution();
+  print(testing.fizzBuzz(100));
+}
