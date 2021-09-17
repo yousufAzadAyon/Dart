8cc1543cd403fc75079418d254413042a7850486
YousudAzadAyon <52939358+yousufAzadAyon@users.noreply.github.com>
2021-09-17 08:53:32 +0600

    madlibs with prompt

--- Dart/tutorial-youtube/madLibs.dart
+++ Dart/tutorial-youtube/madLibs.dart
@@ -1,16 +1,26 @@
 import 'dart:io';
 
+String prompt(String promptText) {
+  print(promptText);
+  String temp = stdin.readLineSync()!;
+  return temp;
+}
+
 void main() {
-  print("enter a color:");
-  String color = stdin.readLineSync()!;
+  // print("enter a color:");
+  // String color = stdin.readLineSync()!;
 
-  print("enter a plural noun:");
-  String pluralNoun = stdin.readLineSync()!;
+  // print("enter a plural noun:");
+  // String pluralNoun = stdin.readLineSync()!;
 
-  print("enter a writer name:");
-  String writerName = stdin.readLineSync()!;
+  // print("enter a writer name:");
+  // String writerName = stdin.readLineSync()!;
+
+  String color = prompt("enter a color: ");
+  String pluralNoun = prompt("enter plural noun: ");
+  String writerName = prompt("enter a writer name: ");
 
   print("roses are $color");
-  print("roses are $pluralNoun");
-  print("roses are $writerName");
+  print("I'm bad at plural noun which are $pluralNoun");
+  print("here is the writer $writerName");
 }
