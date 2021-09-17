b921cad28dec9eb527e4da2132ab8953bbc206e0
YousudAzadAyon <52939358+yousufAzadAyon@users.noreply.github.com>
2021-09-17 09:14:00 +0600

    function

--- Dart/tutorial-youtube/function.dart
+++ Dart/tutorial-youtube/function.dart
@@ -1,3 +1,5 @@
+import 'dart:io';
+
 void drawObjects() {
   print("|-|");
   print("|-|");
@@ -11,8 +13,16 @@ double addNumbers(double number1, double number2) {
   return number1 + number2;
 }
 
+String prompt(String promptText) {
+  print(promptText);
+  String answer = stdin.readLineSync()!;
+  return answer;
+}
+
 void main(List<String> args) {
   drawObjects();
   sayHi("yousuf", 23);
   print(addNumbers(22, 22));
+  String color = prompt("enter a color: ");
+  print("the color is $color");
 }
